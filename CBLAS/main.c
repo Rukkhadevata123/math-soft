a6cf2c78954fc49ee2e37fb74e540dacae056df6
Rukkhadevata123 <3230102179@zju.edu.cn>
2024-04-07 09:32:13 +0800

    main

--- CBLAS/main.c
+++ CBLAS/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <cblas.h>
 #include <time.h>
+#include <stdlib.h>
+
+#define SIZE 1000
 
 void manual_dgemm(int n, double* A, double* B, double* C) {
     int i, j, k;
@@ -17,25 +20,37 @@ void manual_dgemm(int n, double* A, double* B, double* C) {
 
 int main() {
     int i, j;
-    double A[4] = {1.0, 2.0, 3.0, 4.0};  // A = [1 2; 3 4]
-    double B[4] = {5.0, 6.0, 7.0, 8.0};  // B = [5 6; 7 8]
-    double C[4] = {0.0, 0.0, 0.0, 0.0};  // C = [0 0; 0 0]
-    double D[4] = {0.0, 0.0, 0.0, 0.0};  // D = [0 0; 0 0]
+    double *A = (double *)malloc(SIZE * SIZE * sizeof(double));
+    double *B = (double *)malloc(SIZE * SIZE * sizeof(double));
+    double *C = (double *)malloc(SIZE * SIZE * sizeof(double));
+    double *D = (double *)malloc(SIZE * SIZE * sizeof(double));
+
+    for (i = 0; i < SIZE * SIZE; i++) {
+        A[i] = (double)rand() / RAND_MAX;
+        B[i] = (double)rand() / RAND_MAX;
+        C[i] = 0.0;
+        D[i] = 0.0;
+    }
 
     clock_t start, end;
     double cpu_time_used;
 
     start = clock();
-    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, 2, 2, 2, 1.0, A, 2, B, 2, 0.0, C, 2);
+    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, SIZE, SIZE, SIZE, 1.0, A, SIZE, B, SIZE, 0.0, C, SIZE);
     end = clock();
     cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
     printf("Time used by cblas_dgemm: %lf seconds\n", cpu_time_used);
 
     start = clock();
-    manual_dgemm(2, A, B, D);
+    manual_dgemm(SIZE, A, B, D);
     end = clock();
     cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
     printf("Time used by manual_dgemm: %lf seconds\n", cpu_time_used);
+
+    free(A);
+    free(B);
+    free(C);
+    free(D);
 
     return 0;
 }
